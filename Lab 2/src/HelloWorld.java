f2e4d4e3c389128b643d734452ae91a2314c59d3
nedink <nedink@gmail.com>
2014-01-09 15:58:27 -0800

    Lab 2
    "Hello World" Java Program

---
+++ Lab 2/src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, world.");
+		//This is a comment for my HelloWorld program.
+		System.out.println("How are you?");
+	}
+}
